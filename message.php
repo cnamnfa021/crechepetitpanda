a838ab749622a0d565d12b053dca5fba01da5796
marconweb <50239579+marconweb@users.noreply.github.com>
2019-05-08 15:17:17 +0200

    Update message.php
    Correction du problème avec les messages d'erreurs lorsque l'on passe de backoffice.php à message.php (les variables vérifiées n'existent pas si on passe par le backoffice, et la vérification est de ce fait inutile). Cette modification touche aussi backoffice.php et articlemodif.php (page de contact).

--- message.php
+++ message.php
@@ -21,55 +21,66 @@
 <!--Sécurité côté serveur pour le champ user (invalide si vide ou différent du format de la regex, le nombre de caractère est vérifié), 
  même format que dans le js côté client.-->
     <p>
+<!-- $backoffice active la sécurité côté serveur si sa valeur est "ok" (passage par contact.php), et évite l'affichage d'erreurs sinon (par backoffice.php)-->
       <?php
-      $formuser = "valide";
-      $utilisateur = $_GET['user'];
+	  session_start();
+
+      if ($_GET['$backoffice']!="ok"){
+        $formuser = "valide";
+        $utilisateur = $_GET["user"];
         if ('' == $utilisateur) {
           global $formuser;
           $formuser = "vide";
-        echo 'Le champ nom et prénom est : ' . $formuser;
-        }else if ((preg_match("'<|>|{|}|%'", $utilisateur)) or (strlen($utilisateur) < 5)or (strlen($utilisateur) > 25)){
+          echo 'Le champ nom et prénom est : ' . $formuser;
+        }else if ((preg_match("'<|>|{|}|'|%'", $utilisateur)) or (strlen($utilisateur) < 5)or (strlen($utilisateur) > 25)){
           global $formuser;
           $formuser = "invalide";
           echo 'Le champ nom et prénom est : ' . $formuser;
         }
+	  }
       ?>
     </p>
-
-<section id="mentions">
 <!--Sécurité côté serveur pour le champ téléphone (invalide si différent du format de la regex), même format que dans le js côté client.-->
     <p>
-      <?php $telephone = $_GET['telephone'];
-      $formtel = "valide";
-      global $formtel;
-        if (('' == $telephone)) {
+      <?php
+      if ($_GET['$backoffice']!="ok"){
+	    $telephone = $_GET['telephone'];
+        $formtel = "valide";
+        global $formtel;
+        if ('' == $telephone) {
           echo 'Le champ téléphone est vide.';
         }else if (!preg_match("'^[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}$'", $telephone)){
           global $formtel;
           $formtel = "invalide";
           echo 'Le champ téléphone est : ' . $formtel;
         }
+      }
       ?>
     </p>
 <!--Sécurité côté serveur pour le champ message (invalide si vide ou différent du format de la regex, le nombre de caractère est vérifié), 
  même format que dans le js côté client.-->
     <p>
-      <?php $message = $_GET['message'];
-      $formmessage = "valide";
+      <?php
+      if ($_GET['$backoffice']!="ok"){
+         $message = $_GET['message'];
+        $formmessage = "valide";
         if ('' == $message) {
           global $formmessage;
           $formmessage = "vide";
           echo 'Le champ message est vide.';
-         }else if ((preg_match("'<|>|{|}|%'", $message))or (strlen($message) < 10)or (strlen($message) > 150)){
+         }else if ((preg_match("'<|>|{|}|'|%'", $message))or (strlen($message) < 10)or (strlen($message) > 150)){
           global $formmessage;
           $formmessage = "invalide";
           echo 'Le champ message est : ' . $formmessage;
         }
+      }
       ?>
     </p>
 <!--Sécurité côté serveur pour le champ mail (si vide ou différent du format de la regex), même format que dans le js côté client.-->
     <p>
-      <?php $mail = $_GET['mail'];
+      <?php
+      if ($_GET['$backoffice']!="ok"){
+        $mail = $_GET['mail'];
         $formmail = "valide";
         if ('' == $mail) {
           global $formmail;
@@ -80,6 +91,7 @@
           $formmail = "invalide";
           echo 'Le champ mail est : ' . $formmail;
         }
+      }
       ?>
     </p>
 
@@ -87,7 +99,7 @@
 Ici le champs mail est aussi sécurisé, mais le captcha ne l'est pas (il n'est pas demandé et il y dépend de js pour fonctionner).-->
 
     <?php
-      session_start();
+    if ($_GET['$backoffice']!="ok"){
 
 /*la variable $formvalide permet de vérifier que tout les champs sont valides*/
       $formvalide = "invalide";
@@ -96,16 +108,17 @@ Ici le champs mail est aussi sécurisé, mais le captcha ne l'est pas (il n'est 
         global $formvalide;
         $formvalide = "valide";
       }
+    }
 
-		   /* récupération noms des créateures du site de la base de données======================*/ 
-		      include "inc/connexion.inc.php";
+      try{ $bdd = new PDO('mysql:host=localhost;dbname=projetcnam-modif;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); }
+      catch(Exception $e) { die('Erreur : '.$e->getMessage()); }
 
 //Si un administrateur est connecté, la page affiche le contenu de la table message (liste des messages).
       if (! empty($_SESSION["login"]) and ! empty($_SESSION['password'])) {
 
         echo 'Bienvenue ' . $_SESSION["login"];
 
-        $reponse = $con->query('SELECT message_id, message_user, message_message, message_telephone, message_mail, message_sujet, message_date FROM message ORDER BY message_id');
+        $reponse = $bdd->query('SELECT message_id, message_user, message_message, message_telephone, message_mail, message_sujet, message_date FROM message ORDER BY message_id');
 
 //Met en place le tableau html.
         echo '<table><caption><h1>liste des messages : </h1></caption>
@@ -129,13 +142,13 @@ Ici le champs mail est aussi sécurisé, mais le captcha ne l'est pas (il n'est 
 
 /*Si un nouveau message a été enregistré depuis la page de contact, $_GET['user'] n'est pas vide, on affiche donc le message qui a été enregistré.*/
       if (! empty($_GET["user"])) {
-        $reponse = $con->query('SELECT message_id, message_user, message_message, message_telephone, message_mail, message_sujet, message_date FROM message ORDER BY message_id');
-        $st = $con->query("SELECT COUNT(*) FROM message WHERE message_user='".$_GET['user']."' AND message_message='".$_GET['message']."'")->fetch();
+        $reponse = $bdd->query('SELECT message_id, message_user, message_message, message_telephone, message_mail, message_sujet, message_date FROM message ORDER BY message_id');
+        $st = $bdd->query("SELECT COUNT(*) FROM message WHERE message_user='".$_GET['user']."' AND message_message='".$_GET['message']."'")->fetch();
 
 /*$st['COUNT(*)'] == 0 : Cette méthode évite les doublons, mais en tapant le signe ' une erreur s'affiche. -> solution interdire le caractère ' ?*/
 /*Si $formvalide a pour valeur "valide", tout les champs sont valides et le message est enregistré.*/
  if (($st['COUNT(*)'] == 0) and ($formvalide == "valide")){
-        $req = $con->prepare('INSERT INTO message(message_user, message_message, message_telephone, message_mail, message_sujet, message_date) 
+        $req = $bdd->prepare('INSERT INTO message(message_user, message_message, message_telephone, message_mail, message_sujet, message_date) 
         VALUES(:user, :message, :telephone, :mail, :sujet, :date)');
         $req->execute(array(
         'user' => $_GET['user'],
@@ -169,11 +182,12 @@ Ici le champs mail est aussi sécurisé, mais le captcha ne l'est pas (il n'est 
         echo "<p>" . "L'enregistrement dans la bdd est : " . $formvalide . "</p>";
 	  }
 
+/*détruit la variable $backoffice (pour libérer de l'espace et prévenir d'éventuels conflits) ? */
+    unset($backoffice);
+
     ?>
 
     <p><a href="contact.php">Revenir au formulaire de contact</a></p>
-
-</section>
 
     </div>
   </body>
