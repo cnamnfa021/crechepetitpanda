e7563e3cb04abfd9e3e6c6a1ad26f495f4fce79c
Amina ARBANE <iffour.amina@outlook.fr>
2019-05-13 09:58:53 +0200

    Create deconnexion.php

---
+++ deconnexion.php
@@ -0,0 +1,11 @@
+<?php
+// On active les sessions :
+    session_start();
+
+// On detruit les sessions :
+    unset($_SESSION['login'], $_SESSION['password']);
+
+// On redirige le visiteur vers la page désirée :
+    header('Location: ./index.php');
+    exit();
+?>
