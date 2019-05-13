e2c996f396fe1d57cc21a29412924c7320128773
Amina ARBANE <iffour.amina@outlook.fr>
2019-05-13 10:45:10 +0200

    Update conseils-information.php

--- conseils-informations.php
+++ conseils-informations.php
@@ -7,31 +7,54 @@
   </head>
   <body>
     <div id="bloc_page">
+	  <div><!-- Gestion du bouton connexion/déconnexion-->
+		<?php
+		  session_start();
+		  include "inc/connexion.inc.php"; /*connexion à la bdd "projetcnam"*/
+		
+		  if(isset($_SESSION['login'])) {
+		?>
+		  <p id="bienvenu"><!-- affichage du nom de la personne connectée -->
+			<?php echo 'Bienvenue ' . $_SESSION["login"];?>
+		  </p>
+			<!-- Création du bouton déconnexion qui redirige vers la pge déconnexion -->
+			<button class="conex" onClick="location.href='deconnexion.php';">Déconnexion</button>
+			
+		   <?php
+			  }else {
+			?>
+		   <!-- Création du bouton connexion qui redirige vers la page connexion-->
+			 <button class="conex" onClick="location.href='connexion.php';">Connexion</button>
+		
+		   <?php 
+			  }
+		  ?>
+	   </div>
 	
-	<header>
-	   <div id="logo">
-         <img src="images/logo.png" alt="Logo de site" />
-       </div>
-	   <nav>
-         <ul>
-           <li> <a href="index.html"><img id="home" src="images/home.jpg" /></a></li>
-		   <li><a href="conseils-informations.php">Conseils et informations</a></li>
-           <li><a href="journee-type-de-lenfant.php"> Journée type de l'enfant</a></li>
-		   <li><a href="pre-inscription.php"> Prè-inscription </a></li>
-           <li><a href="contact.php">Contact</a></li>
-	     </ul>
-       </nav>
-     </header>
+	  <header>
+		 <div id="logo">
+	       <img src="images/logo.png" alt="Logo de site" />
+		 </div>
+		 <nav>
+		   <ul>
+			  <li> <a href="index.php"><img id="home" src="images/home.jpg" /></a></li>
+			  <li><a href="conseils-informations.php">Conseils et informations</a></li>
+			  <li><a href="journee-type-de-lenfant.php"> Journée type de l'enfant</a></li>
+			  <li><a href="pre-inscription.php"> Pré-inscription </a></li>
+			  <li><a href="contact.php">Contact</a></li>
+		   </ul>
+		 </nav>
+	   </header>
             
 	
-      <div id="entete">
-	    <img id="imgbaniere" src="images/parent.jpg" />
-	  </div>
-	  
-      <div id="titre-principal1">
-         
+	   <div id="entete">
+		<img id="imgbaniere" src="images/parent.jpg" />
+	   </div>
+		  
+	   <div id="titre-principal1">
+			 
 		 <h1> Conseils et informations </h1>    
-    
+		
 	   </div>
    
 	  
@@ -51,7 +74,6 @@
 		 include "inc/connexion.inc.php"; /*connexion à la bdd "projetcnam"*/
          $requete='SELECT * FROM content WHERE content_nompage="Conseils" ORDER BY content_id';//Il faut mettre ORDER BY id pour que les articles s'affichent du premier au dernier posté.
          $reponse=$con->query($requete);
-
         //affiche tout les titres et les descriptions associées.
 		$i=1; //compteur pour incrémenter "id" de div (utilisé dans fichier css)
          while ($donnees = $reponse->fetch()) {
@@ -95,9 +117,3 @@
 	</body>
 	
 </html>
-	
-		
-  
-  
-  
-  
